499ebe29054a48098890d065cd65ad3d761e308e
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-12 19:50:09 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,38 +1,14 @@
 class Book
+  
+  attr_accessor :title, :author, :page_count, :genre
+  
   def initialize (title)
     @title = title
-  end
-  
-  def title
-    @title
-  end
-  
-  def author= (name)
-    @author = name
-  end
-  
-  def author
-    @author 
-  end
-  
-  def page_count= (num)
-    @page_count = num
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre= (type)
-    @genre = type
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
 end
 
